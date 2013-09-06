f9644d1bdbdcd8e5d7be35b77f81ae74fbfb1bd0
improvshark <improvshark@gmail.com>
2013-09-05 23:32:00 -0600

    editing the user profile is styled

--- code/login/views/edit.php
+++ code/login/views/edit.php
@@ -1,58 +1,62 @@
-<!-- this is the Simple sexy PHP Login Script. You can find it on http://www.php-login.net ! It's free and open source. -->
 
-<!-- errors & messages --->
+<div class="container">
+    <div class="jumbotron">
+        <h2>
+            Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in and can edit your credentials here!
+        </h2>
+    </div>
+</div>
 
-<?php
+<div class="container">
+    <!-- edit form for username / this form uses HTML5 attributes, like "required" and type="email" -->
+    <div class="col-md-4">
+        <div class="panel panel-info">
+            <div class="panel-heading"><h4>Username</h4></div>     
+            <div class="panel-body">
+                <form method="post" action="edit.php" name="user_edit_form_name" >
+                <label for="edit_input_username">New Username</label>
+                <input id="edit_input_username" class="login_input form-control form-group" type="text" name="user_name" placeholder="<?php echo $_SESSION['user_name']; ?>" pattern="[a-zA-Z0-9]{2,64}" required />
+                <input type="submit"  class="form-group" name="user_edit_submit_name" value="Change username" />
+                </form>
+            </div>
+        </div>
+    </div>
 
-// show negative messages
-if ($login->errors) {
-    foreach ($login->errors as $error) {
-        echo $error;    
-    }
-}
 
-// show positive messages
-if ($login->messages) {
-    foreach ($login->messages as $message) {
-        echo $message;
-    }
-}
+        <!-- edit form for user email / this form uses HTML5 attributes, like "required" and type="email" -->
+    <div class="col-md-4 ">
+        <div class="panel panel-info">
+            <div class="panel-heading"><h4>Email</h4></div>
+            <div class="panel-body">
+                <form method="post" action="edit.php" name="user_edit_form_email">
+                    <label for="edit_input_email">New email</label><br/>
+                    <input id="edit_input_email" class="login_input form-control form-group" type="email" name="user_email" placeholder="<?php echo $_SESSION['user_email']; ?>" required />
+                    <input type="submit" class="form-group"  name="user_edit_submit_email" value="Change email" />
+                </form>
+            </div>
+        </div>
 
-?>   
+    </div>
 
-<h2>
-    Hey, <?php echo $_SESSION['user_name']; ?>. You are logged in and can edit your credentials here:
-</h2>
 
-<!-- edit form for username / this form uses HTML5 attributes, like "required" and type="email" -->
-<form method="post" action="edit.php" name="user_edit_form_name">
-    <label for="edit_input_username">New username (username cannot be empty and must be azAZ09 and 2-64 characters)</label><br/>
-    <input id="edit_input_username" class="login_input" type="text" name="user_name" pattern="[a-zA-Z0-9]{2,64}" required /> (currently: <?php echo $_SESSION['user_name']; ?>)<br/>
-    <input type="submit"  name="user_edit_submit_name" value="Change username" />
-</form>
+    <!-- edit form for user's password / this form uses the HTML5 attribute "required" -->
+    <div class="col-md-4 ">
+        <div class="panel panel-info">
+            <div class="panel-heading"><h4>Password</h4></div>
+            <div class="panel-body">
+                <form method="post" action="edit.php" name="user_edit_form_password">
+                    <label for="edit_input_password_old">Your OLD Password</label>
+                    <input id="edit_input_password_old" class="login_input form-control form-group" type="password" name="user_password_old" autocomplete="off" />
+                    <label for="edit_input_password_new">Your NEW Password</label>
+                    <input id="edit_input_password_new" class="login_input form-control form-group" type="password" name="user_password_new" autocomplete="off" />
+                    <label for="edit_input_password_repeat">Repeat NEW password</label>
+                    <input id="edit_input_password_repeat" class="login_input form-control form-group" type="password" name="user_password_repeat" autocomplete="off" />
+                    <input type="submit"  name="user_edit_submit_password" class="form-group" value="Change password" />
+                </form>
+            </div>
+        </div>
+    </div>
 
-<!-- edit form for user email / this form uses HTML5 attributes, like "required" and type="email" -->
-<form method="post" action="edit.php" name="user_edit_form_email">
-    <label for="edit_input_email">New email</label><br/>
-    <input id="edit_input_email" class="login_input" type="email" name="user_email" required /> (currently: <?php echo $_SESSION['user_email']; ?>)<br/>
-    <input type="submit"  name="user_edit_submit_email" value="Change email" />
-</form>
-
-<!-- edit form for user's password / this form uses the HTML5 attribute "required" -->
-<form method="post" action="edit.php" name="user_edit_form_password">
-    <label for="edit_input_password_old">Your OLD Password</label>
-    <input id="edit_input_password_old" class="login_input" type="password" name="user_password_old" autocomplete="off" />        
-    <br/>
-    <label for="edit_input_password_new">Your NEW Password</label>
-    <input id="edit_input_password_new" class="login_input" type="password" name="user_password_new" autocomplete="off" />        
-    <br/>
-    <label for="edit_input_password_repeat">Repeat NEW password</label>
-    <input id="edit_input_password_repeat" class="login_input" type="password" name="user_password_repeat" autocomplete="off" />        
-    <br/>
-    <input type="submit"  name="user_edit_submit_password" value="Change password" />
-</form>
-
-<!-- backlink -->
-<a href="index.php">Back to Index Page</a>
-
-<!-- this is the Simple sexy PHP Login Script. You can find it on http://www.php-login.net ! It's free and open source. -->
+    <!-- backlink -->
+    <a href="index.php">Back to Index Page</a>
+</div>
