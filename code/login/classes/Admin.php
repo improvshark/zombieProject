f53c7e3294f54b93f615e1a79d22eac2d5233f53
improvshark <improvshark@gmail.com>
2013-09-07 16:21:59 -0600

    attempting to connect to database with ajax

--- code/login/classes/Admin.php
+++ code/login/classes/Admin.php
@@ -10,6 +10,10 @@ class Admin
 {
     /** @var object $db_connection The database connection */
     private $db_connection = null;
+    /** @var array $errors Collection of error messages */
+    public $errors = array();
+    /** @var array $messages Collection of success / neutral messages */
+    public $messages = array();
 
 
     /**
@@ -66,6 +70,49 @@ class Admin
         }
     }
 
+    public function editUserGroup()
+    {
+        if (!empty($_POST['user_group']) && $_POST['user_email'] == $_SESSION["user_email"]) {
+
+            $this->errors[] = "Sorry, that email address is the same as your current one. Please choose another one.";
+
+        // user mail cannot be empty and must be in email format
+        } elseif (!empty($_POST['user_email']) && filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
+
+            // if database connection opened
+            if ($this->databaseConnection()) {
+
+                // prevent database flooding
+                $this->user_email = substr(trim($_POST['user_email']), 0, 64); 
+                // not really necessary, but just in case...
+                $this->user_id = intval($_SESSION['user_id']);
+
+                // write users new data into database
+                $query_edit_user_email = $this->db_connection->prepare('UPDATE users SET user_email = :user_email WHERE user_id = :user_id');
+                $query_edit_user_email->bindValue(':user_email', $this->user_email, PDO::PARAM_STR);
+                $query_edit_user_email->bindValue(':user_id', $this->user_id, PDO::PARAM_INT);
+                $query_edit_user_email->execute();
+
+                if ($query_edit_user_email->rowCount()) {
+
+                    $_SESSION['user_email'] = $this->user_email;
+                    $this->messages[] = "Your email address has been changed successfully. New email address is " . $this->user_email . ".";
+
+                } else {
+
+                    $this->errors[] = "Sorry, your email changing failed.";
+
+                }
+
+            }
+
+        } else {
+
+            $this->errors[] = "Sorry, your chosen email does not fit into the naming pattern.";
+
+        }
+
+    }  
 
     public function setUserGroup($user_id, $userGroup)
     {
--- code/login/views/admin/user_table.php
+++ code/login/views/admin/user_table.php
@@ -1,3 +1,43 @@
+
+<script type="text/javascript">
+	var changeUser = function(group, name) {
+		console.log( 'change ' + name + 's acount to group ' + group );
+
+		$("#"+name).submit(function(event){
+	    // abort any pending request
+	    if (request) {
+	        request.abort();
+	    }
+	    // setup some local variables
+	    var $form = $(this);
+	    // let's select and cache all the fields
+	    var $inputs = $form.find("input, select, button, textarea");
+	    // serialize the data in the form
+	    var serializedData = $form.serialize();
+
+	    // let's disable the inputs for the duration of the ajax request
+	    $inputs.prop("disabled", true);
+
+
+		request = $.ajax({
+			type: "POST",
+			url: "update.php",
+			data: serializedData,
+	        success: function(){
+            	alert("success");
+            	$("#result").html('Submitted successfully');
+        		},
+        	error:function(){
+            	alert("failure");
+            	$("#result").html('There is error while submit');
+    		}
+
+		})
+	}
+
+
+</script>
+
 <div class="row col-sm-9">
 	<div id="userTable" class=" collapse in">
 		<table class="table table-striped table-bordered table-hover">
@@ -13,11 +53,22 @@
 			<tbody>	
 			<?php
 				foreach ($admin->getAllUserData() as $row) {
-					echo "<tr>";
-					echo "<td>" . $row['user_id'] . "</td>";
-					echo "<td>" . $row['user_name'] . "</td>";
-					echo "<td>" . $row['user_email'] . "</td>";
-					echo "<td>" . $row['user_group'] . "</td>";
+					?>
+					<tr>
+					<td> <?php echo $row['user_id']; ?></td>
+					<td> <?php echo $row['user_name']; ?></td>
+					<td> <?php echo $row['user_email']; ?></td>
+					 <td>
+					 	<form id="<?php echo $row['user_name']; ?>">
+						 	<select class="btn-sm btn-info" onchange="changeUser(this.options[this.selectedIndex].value, '<?php echo $row['user_name']; ?>')">
+								<option <?php if ($row['user_group'] == 'user'){ echo "selected='selected'";}?> >user</option>
+								<option <?php if ($row['user_group'] == 'dev'){ echo "selected='selected'";}?>>dev</option>
+								<option <?php if ($row['user_group'] == 'admin'){ echo "selected='selected'";}?>>admin</option>
+							</select>
+						</form>
+					</td>
+
+					 <?php
 				}
 			?>
 			</tbody>
