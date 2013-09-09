1842eaba7ea34156f949d873b172f2935d1bad37
improvshark <moonyou1234321@gmail.com>
2013-09-09 12:50:08 -0600

    tweeking style on admin page

--- code/login/admin_index.php
+++ code/login/admin_index.php
@@ -31,17 +31,23 @@
 
                 <div id="myTabContent" class="tab-content">
                     <div class="tab-pane fade active in" id="users">
-                        <?php include("views/admin/user_table.php"); ?>
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <?php include("views/admin/user_table.php"); ?>
+                            </div>
+                        </div>
                     </div>
                     <div class="tab-pane fade" id="profile">
-                        <p>
-                            Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, 
-                            blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, 
-                            commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic
-                            magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum
-                            wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester 
-                            stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.
-                        </p>
+                        <div class="panel panel-default">
+                            <p class="panel-body">
+                                Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, 
+                                blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, 
+                                commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic
+                                magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum
+                                wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester 
+                                stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.
+                            </p>
+                        </div>
                     </div>
          
                 </div>
