60dda69b96ec800a8e8b720e6f06ce2f33f85e41
improvshark <improvshark@gmail.com>
2013-09-21 21:57:17 -0600

    added a link to footer to map editor for ease

--- code/views/templates/footer.php
+++ code/views/templates/footer.php
@@ -1,4 +1,7 @@
 
     <footer class="container">
-        <p><a href="register.php" class="text-muted">New account / </a> <a href="password_reset.php" class="text-muted"> forgot password</a></p>
+        <p>
+        	<a href="register.php" class="text-muted">New account / </a> 
+        	<a href="password_reset.php" class="text-muted"> forgot password / </a>
+        	<a href="editor.php" class="text-muted"> map Editor</a></p>
     </footer>
